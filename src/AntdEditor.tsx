a092b1de7febae62c05030116e58f6512c98ff56
root <root@node15.bluemix.net>
2020-02-26 22:09:55 -0500

    antd demo

---
+++ src/AntdEditor.tsx
@@ -0,0 +1,70 @@
+import React, { PureComponent } from 'react';
+// import { FormField } from '@grafana/ui';
+import { PanelEditorProps } from '@grafana/data';
+
+import { AntdOptions } from './types';
+import { Table } from 'antd';
+
+const columns = [
+  { title: 'Name', dataIndex: 'name', key: 'name' },
+  { title: 'Age', dataIndex: 'age', key: 'age' },
+  { title: 'Address', dataIndex: 'address', key: 'address' },
+  {
+    title: 'Action',
+    dataIndex: '',
+    key: 'x',
+    render: () => <a>Delete</a>,
+  },
+];
+
+const data = [
+  {
+    key: 1,
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    description: 'My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.',
+  },
+  {
+    key: 2,
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    description: 'My name is Jim Green, I am 42 years old, living in London No. 1 Lake Park.',
+  },
+  {
+    key: 3,
+    name: 'Not Expandable',
+    age: 29,
+    address: 'Jiangsu No. 1 Lake Park',
+    description: 'This not expandable',
+  },
+  {
+    key: 4,
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    description: 'My name is Joe Black, I am 32 years old, living in Sidney No. 1 Lake Park.',
+  },
+];
+
+export class AntdEditor extends PureComponent<PanelEditorProps<AntdOptions>> {
+  onTextChanged = ({ target }: any) => {
+    this.props.onOptionsChange({ ...this.props.options, text: target.value });
+  };
+
+  render() {
+    // const { options } = this.props;
+
+    return (
+      <Table
+        columns={columns}
+        expandable={{
+          expandedRowRender: record => <p style={{ margin: 0 }}>{record.description}</p>,
+          rowExpandable: record => record.name !== 'Not Expandable',
+        }}
+        dataSource={data}
+      />
+    );
+  }
+}
